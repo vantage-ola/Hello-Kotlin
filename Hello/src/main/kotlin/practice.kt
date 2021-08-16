679dbb1c86ba045b068fa17f13b7c1edba63f711
vantage-ola <vantageola4@gmail.com>
2021-08-16 17:01:40 +0100

    16/08/2021

---
+++ Hello/src/main/kotlin/practice.kt
@@ -0,0 +1,7 @@
+val battleOfHastings: Int = 1066
+val pi: Float = 3.14f
+var worldRecord100m: Float = 9.63f
+var millisecondsSince1970: Long = 1544693462311
+// True at 9:30am 13/12/2018
+val beerIsTasty: Boolean = true
+var isItRaining: Boolean = false
